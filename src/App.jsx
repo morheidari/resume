a0d761d3639927003fa734350b83d1ffeac3a1b1
mohammadreza <moreh412000@gmial.com>
2024-08-21 02:42:46 +0330

    create basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header'
+import Main from './components/main'
+import SideBar from './components/sidebar'
 
 function App() {
 
+  return(<div className='app'><Header></Header><SideBar></SideBar><Main></Main></div>)
 }
 
 export default App
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export default function SideBar(){
+    const [isOpen, ChangeOpennes] = useState(true)
+    function onClick(){
+        ChangeOpennes(!isOpen)
+    }
+    function returnClass(isOpen){
+        if(isOpen)return 'open'
+        else return 'close'
+    }
+    return(
+        <><aside id="edit-side-bar">
+        <button className={`edit-btn + ${returnClass(isOpen)}`} onClick={onClick}><ArrowIcon></ArrowIcon></button>
+        </aside>
+        </>
+    )
+}
+
+function ArrowIcon(){
+    return(
+      <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M30.83 32.67l-9.17-9.17 9.17-9.17-2.83-2.83-12 12 12 12z"/><path d="M0-.5h48v48h-48z" fill="none"/></svg>
+    )
+}
